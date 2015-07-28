f6b3e310c30b8c8638e96946785351992ef52a57
fannon <heimlersimon@gmail.com>
2015-07-28 11:43:26 +0200

    * Fixing EL index must be lowercase

--- src/target/elasticsearchTarget.js
+++ src/target/elasticsearchTarget.js
@@ -6,12 +6,14 @@ exports.client = undefined;
 
 exports.init = function(settings, callback) {
 
+    var indexId = 'cacheur_' + settings.id.toLowerCase();
+
     var elSettings = {
         host: settings.elasticsearch.host || 'localhost:9200'
     };
 
     if (settings.verbose) {
-        log('[i] ElasticSearch initializing new index: cacheur_' + settings.id);
+        log('[i] ElasticSearch initializing new index: ' + indexId);
     }
 
     if (settings.debug) {
@@ -21,11 +23,11 @@ exports.init = function(settings, callback) {
     exports.client = elasticsearch.Client(elSettings);
 
     exports.client.indices.delete({
-        index: 'cacheur_' + settings.id
+        index: indexId
     }, function(err, resp) {
 
         if (err) {
-            log('[E] Error while deleting the ElasticSearch index: cacheur_' + settings.id);
+            log('[E] Error while deleting the ElasticSearch index: ' + indexId);
             log(err);
         }
 
@@ -40,7 +42,7 @@ exports.init = function(settings, callback) {
         };
 
         exports.client.indices.create({
-            index: 'cacheur_' + settings.id,
+            index: indexId,
             body: indexSettings
         }, callback);
     });
@@ -56,9 +58,10 @@ exports.sync = function(settings, diff) {
     var i;
     var id;
     var bulkJobs = '';
+    var indexId = 'cacheur_' + settings.id.toLowerCase();
 
     if (settings.verbose) {
-        log('[i] ElasticSearch syncing index: cacheur_' + settings.id);
+        log('[i] ElasticSearch syncing index: ' + indexId);
     }
 
     if (!exports.client) {
@@ -68,7 +71,7 @@ exports.sync = function(settings, diff) {
     // Remove Documents
     for (i = 0; i < diff.removed.length; i++) {
         id = diff.removed[i];
-        bulkJobs += '{ "delete" : { "_index" : "cacheur_' + settings.id + '", "_type" : "cacheur", "_id" : "' + id + '" } }\n';
+        bulkJobs += '{ "delete" : { "_index" : "' + indexId + '", "_type" : "cacheur", "_id" : "' + id + '" } }\n';
     }
 
     // Create Documents
@@ -76,7 +79,7 @@ exports.sync = function(settings, diff) {
         var add = diff.added[i];
         id = add[settings.diff.id];
 
-        bulkJobs += '{ "create" : { "_index" : "cacheur_' + settings.id + '", "_type" : "cacheur", "_id" : "' + id + '" } }\n';
+        bulkJobs += '{ "create" : { "_index" : "' + indexId + '", "_type" : "cacheur", "_id" : "' + id + '" } }\n';
         bulkJobs += JSON.stringify(add) + '\n';
     }
 
@@ -84,7 +87,7 @@ exports.sync = function(settings, diff) {
     for (i = 0; i < diff.changed.length; i++) {
         var change = diff.changed[i];
         id = change[settings.diff.id];
-        bulkJobs += '{ "index" : { "_index" : "cacheur_' + settings.id + '", "_type" : "cacheur", "_id" : "' + id + '" } }\n';
+        bulkJobs += '{ "index" : { "_index" : "' + indexId + '", "_type" : "cacheur", "_id" : "' + id + '" } }\n';
         bulkJobs += JSON.stringify(change) + '\n';
     }
 
